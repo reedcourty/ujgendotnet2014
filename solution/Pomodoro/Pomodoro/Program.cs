0b1ba0db971e8f59c0bcd370c7c148f4b4d9a161
reedcourty <reedcourty@gmail.com>
2014-12-12 22:06:47 +0100

    Egy egyszerű Rx-es timer került be a Program.cs-be.

--- solution/Pomodoro/Pomodoro/Program.cs
+++ solution/Pomodoro/Pomodoro/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace Pomodoro
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+
+            var observable = Observable.Timer(DateTime.Now + TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(1)).Timestamp();
+
+            using (observable.Subscribe(
+                x => Console.WriteLine("{0}: {1}", x.Value, x.Timestamp)))
+            {
+                Console.WriteLine("Press any key to unsubscribe");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
