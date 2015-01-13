7ed431b649dc961cf6413edc06884b7fe04818da
reedcourty <reedcourty@gmail.com>
2015-01-13 19:44:16 +0100

    Eltávolítottam a felesleges commenteket.

--- solution/Pomodoro/PomodoroService/IPomodoroService.cs
+++ solution/Pomodoro/PomodoroService/IPomodoroService.cs
@@ -11,57 +11,27 @@ namespace PomodoroService
     [ServiceContract]
     public interface IPomodoroService
     {
-        /*
-        [OperationContract]
-        List<Tag> GetTags();
-
-        [OperationContract]
-        List<Comment> GetComments();
-
-
-        [OperationContract]
-        Tag GetTag(int id);
-        */
-
         [OperationContract]
         Entry GetEntryById(int id);
 
-        /*
-        [OperationContract]
-        Comment GetComment(int id);
-        */
 
         [OperationContract]
         Tag AddTag(string tagName);
 
-        /*
-        [OperationContract]
-        void AddEntry(Entry new_entry);
-
-
-        [OperationContract]
-        void AddComment(Comment new_comment);
-
-
-        [OperationContract]
-        void UpdateTag(int id, string tagname, List<int> entry_ids);
-
-        */
+        
         [OperationContract]
         string OldUpdateEntry(int entryId, string oldDescription, string newDescription);
+
 
         [OperationContract]
         [FaultContract(typeof(FaultException))]
         Entry UpdateEntry(Entry newEntry);
 
-        /*
-        [OperationContract]
-        void UpdateComment(int id, string content, int entryId);
-        */
-
+        
         [OperationContract()]
         [FaultContract(typeof(FaultException))]
         void SaveEntry(DateTime timestamp, string description, string tags);
+
 
         [OperationContract]
         [FaultContract(typeof(FaultException))]
--- solution/Pomodoro/PomodoroService/PomodoroService.cs
+++ solution/Pomodoro/PomodoroService/PomodoroService.cs
@@ -13,63 +13,23 @@ namespace PomodoroService
 
         private PomodoroDAL.Methods methods = new Methods();
 
-        /*
-        public List<Tag> GetTags()
-        {
-            return new List<Tag>();
-        }
 
-        public List<Comment> GetComments()
-        {
-            return new List<Comment>();
-        }
-
-
-        public Tag GetTag(int id)
-        {
-            return new Tag();
-        }
-
-        */
         public Entry GetEntryById(int id) {
             return methods.GetEntryById(id);
         }
 
-        /*
-        public Comment GetComment(int id)
-        {
-            return new Comment();
-        }
-
-        */
-
+        
         public Tag AddTag(string tagName)
         {
             return methods.AddNewTag(tagName);
         }
 
-        /*
-        public void AddEntry(Entry new_entry)
-        {
-
-        }
-
-
-        public void AddComment(Comment new_comment)
-        {
-
-        }
-
-        public void UpdateTag(int id, string tagname, List<int> entry_ids)
-        {
-
-        }
-        */
-
+        
         public string OldUpdateEntry(int entryId, string oldDescription, string newDescription)
         {
             return methods.UpdateEntry(entryId, oldDescription, newDescription);
         }
+
 
         public Entry UpdateEntry(Entry newEntry)
         {
@@ -90,12 +50,7 @@ namespace PomodoroService
             return result;
         }
 
-        /*
-        public void UpdateComment(int id, string content, int entryId)
-        {
-
-        }
-        */
+       
         public void SaveEntry(DateTime timestamp, string description, string tags)
         {
             try
@@ -107,6 +62,7 @@ namespace PomodoroService
                 throw new FaultException(String.Format("Something bad happend while we're trying to save the entry! :( ({0})", ie.Message));
             }
         }
+
 
         public List<Entry> GetEntries()
         {
